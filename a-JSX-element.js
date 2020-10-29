8e7111b3862f8b886914a331258f452672c1e9a4
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-10-29 14:29:29 +0530

    Create a-JSX-element.js

---
+++ a-JSX-element.js
@@ -0,0 +1,19 @@
+/*
+
+A basic JSX element declared and defined in JS: assignig an 'h1' (element of HTML).
+
+*/
+
+const JSX = <h1>Hello JSX!</h1>;
+
+console.log(JSX);
+/*
+The log prints out the folowing:
+
+{ '$$typeof': {},
+  type: 'h1',
+  key: null,
+  ref: null,
+  props: { children: 'Hello JSX!' },
+  _owner: null }
+*/
