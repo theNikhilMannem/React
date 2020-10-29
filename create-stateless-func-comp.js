78e544eb713dbccc966e0f21ef8fb54a40f41d2e
Nikhil M Kumar <38357711+theNikhilMannem@users.noreply.github.com>
2020-10-29 20:05:30 +0530

    Create create-stateless-func-comp.js

---
+++ create-stateless-func-comp.js
@@ -0,0 +1,15 @@
+/*
+
+Components are the core of React. Here, we have a demo of how to create a Stateless Functional Component using JavaScript function.
+
+Note that the component should return either "JSX" or "null".
+
+*/
+
+
+const MyComponent = function() {
+  // Returns JSX.
+  return (
+    <div> A String </div>
+  );
+}
